71540159be27702948fd94beb71cabba744713cb
Игорь Звягинцев <i.zvyagintsev@rambler-co.ru>
2018-01-24 16:19:34 +0300

    fixes

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const mapArrayToObject = (array, key) => {
-    const objects = {}
-    array.forEach(object => {
-        objects[object[key]] = object
-    })
-    return objects
+    const objects = Object.create(null) // entirely clean object w/o __proto__ etc
+
+    if ([] instanceof Array && typeof key == 'string') {
+        array.forEach(object => {
+            objects[object[key]] = object
+        })
+        return objects
+    } else {
+        console.warn('mapArrayToObject: Check the arguments passed! One or more are wrong!')
+    }
 }
 
-export mapArrayToObject
+module.exports = mapArrayToObject
